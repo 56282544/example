0512e1e63e6afee0c701b0a7907c922b5ca39977
淦汤 刘 <15116818275@139.com>
2017-06-19 19:08:54 +0800

    添加项目文件。

---
+++ example/js/main.js
@@ -0,0 +1,9 @@
+﻿// 在此放置代码!
+var canvas = document.getElementById("canvas");
+context = canvas.getContext("2d");
+
+context.font = "38pt Arial";
+context.fillStyle = "cornflowerblue";
+context.strokeStyle = "blue";
+context.fillText("hello, world", canvas.width / 2 - 150, canvas.height / 2 + 15);//填充颜色
+context.strokeText("hello, world", canvas.width / 2 - 150, canvas.height / 2 + 15);//边缘描绘
